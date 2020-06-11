02e2527ef46e0988320156dd4221fc870db870e9
Quentin Roy-Foster <16qrf1@caslab.queensu.ca>
2020-06-10 23:09:15 -0400

    polishing home page

--- react-web-bp/src/components/Navigation/index.js
+++ react-web-bp/src/components/Navigation/index.js
@@ -1,82 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SignOutButton from '../SignOut';
-import * as ROUTES from '../../constants/routes';
-import { AuthUserContext } from '../Session';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import { Link } from "react-router-dom";
+import SignOutButton from "../SignOut";
+import * as ROUTES from "../../constants/routes";
+import { AuthUserContext } from "../Session";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
 import Container from "@material-ui/core/Container";
 import {
   makeStyles,
   ThemeProvider,
-  createMuiTheme
+  createMuiTheme,
 } from "@material-ui/core/styles";
-import Roboto from 'fontsource-roboto';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import Badge from '@material-ui/core/Badge';
-import '../Firebase/firebase';
-import { useHistory, Redirect } from 'react-router-dom';
-
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import "../Firebase/firebase";
+import { useHistory, Redirect } from "react-router-dom";
+import useStyles from "../../styles/styles";
 
 const theme = createMuiTheme({
   mixins: {
     denseToolbar: {
-      minHeight: 40
-    }
-  }
+      minHeight: 40,
+    },
+  },
 });
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    color: "white"
-  },
-  title: {
-    flexGrow: 1,
-    marginRight: theme.spacing(1),
-    color: "white",
-  },
-  offset: theme.mixins.denseToolbar
-}));
-
 //When Authenticated
-const NavigationAuth = ( props ) => {
-  const classes = useStyles();
+const NavigationAuth = (props) => {
   const history = useHistory();
-  
+  const classes = useStyles();
 
-  return(
-      <>
-          <Toolbar variant="dense" style={{background: 'linear-gradient(45deg, rgba(0,114,255,1) 0%, rgba(11,141,255,1) 100%)'}}>
-            <IconButton
-            component={Link} to={ROUTES.HOME}
-              className={classes.menuButton}
-              aria-label="menu"
-              edge="start"
-            >
-              <MenuIcon color="white"/>
-            </IconButton>
-            <Typography variant="h5" className={classes.title} >
-              QTMA Web App Boiler Plate
-            </Typography>
-            <IconButton component={Link} to={ROUTES.PROFILE} aria-label="show 17 new notifications" className={classes.menuButton}>
-                <AccountCircle />
-            </IconButton>
-            <SignOutButton />
-          </Toolbar>
-        <div className={classes.offset} />
-        
-      </>
+  return (
+    <>
+      <Toolbar
+        variant="dense"
+        style={{
+          background:
+            "linear-gradient(45deg, rgba(0,114,255,1) 0%, rgba(11,141,255,1) 100%)",
+        }}
+      >
+        <Button
+          variant="outlined"
+          component={Link}
+          to={ROUTES.HOME}
+          className={classes.menuButton}
+        >
+          QTMA
+        </Button>
+
+        <IconButton
+          component={Link}
+          to={ROUTES.PROFILE}
+          aria-label="show 17 new notifications"
+          className={classes.menuButton}
+        >
+          <AccountCircle />
+        </IconButton>
+        <SignOutButton />
+      </Toolbar>
+      <div className={classes.offset} />
+    </>
   );
-} 
+};
 
 // function goHome(props) {
 //   const history = useHistory();
@@ -84,34 +73,35 @@ const NavigationAuth = ( props ) => {
 // }
 
 //When Not Authenticated
-const NavigationNonAuth = props => {
+const NavigationNonAuth = (props) => {
   const classes = useStyles();
   const history = useHistory();
-  return(
+  return (
     <>
-            
-          <Toolbar variant="dense" style={{background: 'linear-gradient(45deg, rgba(0,114,255,1) 0%, rgba(11,141,255,1) 100%)'}}>
-            <Typography variant="h5" className={classes.title} >
-              QTMA Web App Boiler Plate
-            </Typography>
-            <Button variant="outlined" component={Link} to={ROUTES.LOGIN}>
-              Log In
-            </Button>
-          </Toolbar>
+      <Toolbar
+        variant="dense"
+        style={{
+          background:
+            "linear-gradient(45deg, rgba(0,114,255,1) 0%, rgba(11,141,255,1) 100%)",
+        }}
+      >
+        <Typography variant="h5" className={classes.title}>
+          QTMA Web App Boiler Plate
+        </Typography>
+        <Button variant="outlined" component={Link} to={ROUTES.LOGIN}>
+          Log In
+        </Button>
+      </Toolbar>
 
-        <div className={classes.offset} />
-        
-      </>
+      <div className={classes.offset} />
+    </>
   );
-}
-
+};
 
 export default function Navigation() {
-  return(
+  return (
     <AuthUserContext.Consumer>
-    {authUser =>
-      authUser ? <NavigationAuth />: <NavigationNonAuth />
-    }
-  </AuthUserContext.Consumer>
+      {(authUser) => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
+    </AuthUserContext.Consumer>
   );
 }
--- react-web-bp/src/components/SignOut/index.js
+++ react-web-bp/src/components/SignOut/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { withFirebase } from '../Firebase';
-import Button from '@material-ui/core/Button';
-
+import React from "react";
+import { withFirebase } from "../Firebase";
+import Button from "@material-ui/core/Button";
 
 const SignOut = ({ firebase }) => (
   <Button variant="outlined" onClick={firebase.signOut}>
     Log Out
-    </Button>
+  </Button>
 );
- 
+
 export default withFirebase(SignOut);
